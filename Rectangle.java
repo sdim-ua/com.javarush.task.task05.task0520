0747081926a833e3dd9c7bef78b1e2bff8606a22
sdim-ua <ingersol@yandex.ua>
2019-01-29 23:38:54 +0400

    Create Rectangle.java

---
+++ Rectangle.java
@@ -0,0 +1,42 @@
+/* 
+Создать класс прямоугольник (Rectangle)
+*/
+
+
+public class Rectangle {
+    //напишите тут ваш код
+    public class Rectangle {
+    //напишите тут ваш код
+    int top;
+    int left;
+    int width;
+    int height;
+    
+    public Rectangle (int top, int left, int width, int height){
+    this.top = top;
+    this.left = left;
+    this.width = width;
+    this.height = height;
+    }
+    
+    public Rectangle(int top, int left, int height){
+    this.top = top;
+    this.left = left;
+    this.height = height;
+    }
+    
+     public Rectangle (int top, int left){
+    this.top = top;
+    this.left = left;
+    }
+    
+    public Rectangle(int top){
+    this.top = top;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
+   
